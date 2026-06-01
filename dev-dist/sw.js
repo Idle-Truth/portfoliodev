139db4b1531ff91c2082b47826e753791fc686f1
Taylor Harry <160172443+Idle-Truth@users.noreply.github.com>
2026-06-01 17:37:48 +0000

    Adding XR site icon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m4pck3qf8us"
+    "revision": "0.036arkhqjlk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
